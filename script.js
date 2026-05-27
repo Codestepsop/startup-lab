0dda2f6274f5a0ff5cbade31855554fce436183c
Gustavo Passos <gustavo.csvp@gmail.com>
2026-05-27 19:16:36 -0300

    adiciona logica de agendamento

--- script.js
+++ script.js
@@ -7,11 +7,27 @@ botao.addEventListener("click", function () {
 
 const form = document.getElementById("formAgenda");
 const resposta = document.getElementById("resposta");
+const listaAgendamentos = document.getElementById("listaAgendamentos");
+const agendamentos = [];
 
-const ultimoAgendamento = localStorage.getItem("ultimoAgendamento");
+function renderizarAgendamentos() {
+  listaAgendamentos.innerHTML = "";
 
-if (ultimoAgendamento) {
-  resposta.textContent = ultimoAgendamento;
+  agendamentos.forEach(function(agendamento, index) {
+    listaAgendamentos.innerHTML += `
+      <li>
+       ${agendamento.horario} - ${agendamento.nome} - ${agendamento.servico}
+        <button onclick="removerAgendamento(${index})">
+          Remover
+        </button>
+      </li>
+    `;
+  });
+}
+
+function removerAgendamento(index) {
+  agendamentos.splice(index, 1);
+  renderizarAgendamentos();
 }
 
 form.addEventListener("submit", function(event) {
@@ -19,14 +35,39 @@ form.addEventListener("submit", function(event) {
 
   const nome = document.getElementById("nome").value;
   const servico = document.getElementById("servico").value;
+  const horario = document.getElementById("horario").value;
 
-  if (nome === "" || servico === "") {
+if (
+  nome === "" ||
+  servico === "" ||
+  horario === ""
+  ) 
+  {
     resposta.textContent = "Preencha nome e serviço antes de agendar.";
     return;
+}
+const horarioExistente = agendamentos.find(
+  function(agendamento) {
+    return agendamento.horario === horario;
   }
+ );
 
+if (horarioExistente) {
+
+  resposta.textContent =
+    "Esse horário já está ocupado.";
+
+  return;
+}
   const texto = `${nome}, seu agendamento para ${servico} foi solicitado 🚀`;
 
   resposta.textContent = texto;
-  localStorage.setItem("ultimoAgendamento", texto);
+
+  agendamentos.push({
+  nome: nome,
+  servico: servico,
+  horario: horario
+});
+
+  renderizarAgendamentos();
 });
