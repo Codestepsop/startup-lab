175c7e83bbb2ad63ef36ccab71bbde5426da3315
Gustavo Passos <gustavo.csvp@gmail.com>
2026-05-27 13:09:34 -0300

    adiciona formulário de agendamento

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ const mensagem = document.getElementById("mensagem");
 botao.addEventListener("click", function () {
   mensagem.textContent = "Primeira interação com JavaScript 🚀";
 });
+
+const form = document.getElementById("formAgenda");
+const resposta = document.getElementById("resposta");
+
+const ultimoAgendamento = localStorage.getItem("ultimoAgendamento");
+
+if (ultimoAgendamento) {
+  resposta.textContent = ultimoAgendamento;
+}
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+  const servico = document.getElementById("servico").value;
+
+  if (nome === "" || servico === "") {
+    resposta.textContent = "Preencha nome e serviço antes de agendar.";
+    return;
+  }
+
+  const texto = `${nome}, seu agendamento para ${servico} foi solicitado 🚀`;
+
+  resposta.textContent = texto;
+  localStorage.setItem("ultimoAgendamento", texto);
+});
